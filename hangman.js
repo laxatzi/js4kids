9fbf1f80e42912bfadc59dabef9ab94fe65c2999
laxatzi <laxatzi@yahoo.com>
2019-07-31 13:15:27 +0300

    comment first step in game building

--- hangman.js
+++ hangman.js
@@ -1,28 +1,54 @@
 // a pseudocode version of our game
 
  /*
-      Pick a random word
+     1. Pick a random word
 
       While the word has not been guessed {
-         Show the player their current status
-         Get a guess from the player
+        2. Show the player their current progress
+        3. Get a guess from the player
 
-         if the player wants to quit {
+       4.  if the player wants to quit {
             quit
          }
-         else if the guess is not valid {
+       5.  else if the guess is not valid {
             Tell player to pick a single letter
          }
 
-         else {
+      6.   else {
             if the guess is in the word {
                update players progress by adding the guess
             }
          }
       }
-         congrats the player on guessing the word
+      7.   congrats the player on guessing the word
  */
 
- 
+// The main game takes place in a while loop in which the basic for all games structure exists, thus, 
+//  a. Take input from player
+//  b. Update games state
+//  c. Display current state to the player
+
+
+// 1. Will "feed" the game with some words and then let it "choose" one randomly. This is gonna be our hangman word
+
+     var words = [
+         "Thessaloniki",
+         "Livorno",
+         "Salerno",
+         "Venice",
+         "Athens",
+         "Heraklion",
+         "Istanbul"
+     ]
+
+     var selectedWord = words[Math.floor(Math.random() * words.length)];
+
+// 2. =>  We will show an answer array of blanks in the place of each letter __ When a letter is guessed, it wil replace a blank.
+// We will also use a variable to keep tracking how many letters are still left after each correct guess. For every guess the variable will be decreased by one (--variable)
+    
+     // set up answer array
+     var answerArr = [];
+
+
 
 
