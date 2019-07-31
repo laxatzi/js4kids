87d9de69d8e988d778b3c3adadc30a6b504940ba
laxatzi <laxatzi@yahoo.com>
2019-07-31 12:54:09 +0300

    add pseudocode in hangman.js file

--- hangman.js
+++ hangman.js
@@ -1,1 +1,28 @@
+// a pseudocode version of our game
+
+ /*
+      Pick a random word
+
+      While the word has not been guessed {
+         Show the player their current status
+         Get a guess from the player
+
+         if the player wants to quit {
+            quit
+         }
+         else if the guess is not valid {
+            Tell player to pick a single letter
+         }
+
+         else {
+            if the guess is in the word {
+               update players progress by adding the guess
+            }
+         }
+      }
+         congrats the player on guessing the word
+ */
+
+ 
+
 
