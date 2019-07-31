93856459ba80d4f892fca8caa8335b32bfbda0df
laxatzi <laxatzi@yahoo.com>
2019-07-31 10:25:32 +0300

    complete exercise #3

--- jskids.js
+++ jskids.js
@@ -15,14 +15,28 @@ console.log(randomString);
 
 // Exercise #3
 // Turn text into h4ck3r sp34k!
+// letters to replace: i=1, a=4, e=3, o=0;
 
 var input = "javascript is awesome!";
 var output= "";
 
 // The actual loop
    for(var j =0; j<input.length; j++){
+      if (input[j]=== "i"){
+         output += 1;
+      }
+      else if(input[j] === "a"){
+         output += 4;
+      }
+      else if (input[j] === "e"){
+         output += 3;
+      }
+      else if (input[j]=== "o"){
+         output += 0;
+      }
+      else{
       output += input[j];
    }
-
+}
 //log to check
-// console.log(output);
+ console.log(output);
