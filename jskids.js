43c42032d9eda4b0707578f2690483a15e0a13cc
laxatzi <laxatzi@yahoo.com>
2019-07-31 10:00:16 +0300

    complete loop exercise

--- jskids.js
+++ jskids.js
@@ -1,1 +1,8 @@
+var animals = ["lion", "wolf", "tiger", "elephant"];
+
+for(var j =0; j<animals.length; j++){
+   var almighty = "Almighty ";
+   animals[j] = almighty +animals[j];
+}
+console.log(animals);
 
