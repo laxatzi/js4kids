15f4beb19bf44650e022661d5ac4c99a58c0ba20
laxatzi <laxatzi@yahoo.com>
2019-08-01 13:35:16 +0300

    example of breaking a function with return

--- return.js
+++ return.js
@@ -1,1 +1,16 @@
+// using return to leave from a function
+
+var fifthLetterOfName = function(name) {
+   if(name.length < 5) {
+      return;
+   }
+
+   return "The fifth letter of your name is "+ name[4]+"!";
+}
+
+console.log(
+            fifthLetterOfName("Stavros")
+);
+
+// return INSTEAD OF if...else
 
