2222bbdda17327bf7c350caa0a1fbda12967aa6c
laxatzi <laxatzi@yahoo.com>
2019-08-01 12:17:46 +0300

    pick randoms

--- functions.js
+++ functions.js
@@ -4,3 +4,13 @@
 var randomBodyParts = ["nose", "hair", "neck"];
 var randomAdjectives= ["smelly", "filthy", "boring"];
 var randomAnimals =["pig", "monkey", "cow"];
+
+// pick a random body part
+var aRandomBodyPart = randomBodyParts[Math.floor(Math.random()*randomBodyParts.length)];
+
+// pick a random adjective 
+var aRandomAdjective = randomAdjectives[Math.floor(Math.random()* randomAdjectives.length)];
+
+// pick a random animal
+var aRandomAnimal = randomAnimals[Math.floor(Math.random()*randomAnimals.length)];
+
