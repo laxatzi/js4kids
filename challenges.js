4600fbfb42787157aa3dd94ddd1802bbe3eb4f18
laxatzi <laxatzi@yahoo.com>
2019-08-01 15:57:29 +0300

    solving the second challenge

--- challenges.js
+++ challenges.js
@@ -24,3 +24,26 @@ console.log(
 //   of numbers as arguments. It should return true if the two
 //   arrays are the same (that is, they have the same numbers
 //   in the same order) and false if they’re different
+
+var num1 = [3, 8, 9];
+var num2 = [3, 8, 9];
+var num3 = [7, 0, 1];
+
+var areArraysSame = function(arr1, arr2){
+   for(var i =0; i<arr1.length; i++){ // for loop to check objects in first array
+      for(var j=0; j<arr2.length; j++) { // for loop to check objects in second array
+           // conditional statement for comparing the two arrays object to object
+        if (arr1[i] !== arr2[j]) {
+           return false;
+        }
+          return true;
+      }
+   }
+}
+
+console.log(
+   areArraysSame(num1,num2)  // true
+); 
+console.log(
+   areArraysSame(num1,num3) // false
+);
