f895e0703215c4e5bd13470f93935e5d6cd57e1c
laxatzi <laxatzi@yahoo.com>
2019-08-01 15:19:50 +0300

    solve first challenge

--- challenges.js
+++ challenges.js
@@ -1,1 +1,18 @@
+// Create two functions, add and multiply.
+// Using only these two functions, solve this simple
+// mathematical problem:
+// 36325 * 9824 + 777
+
+var multiply = function(a,b){
+   return a *b;
+}
+
+var add = function(a,b){
+   return a+b;
+}
+
+console.log(
+   add(multiply(36325, 9824),777)
+);
+
 
