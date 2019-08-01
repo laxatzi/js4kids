d267153e45ef8d0191cded2cc073a08b1d3120c7
laxatzi <laxatzi@yahoo.com>
2019-08-01 17:17:41 +0300

    create new element and add it to the DOM tree

--- dom_mod.js
+++ dom_mod.js
@@ -1,6 +1,16 @@
+
+//replace heading text using the DOM.
+
 var headingOne = document.getElementById('main_heading');
 var headingText = "This is the new heading text!";
 
 headingOne.innerHTML = headingText;
 
+// create new elements and add them to the DOM tree
+
+var newParText = document.createTextNode("This is a new paragraph"); // paragraph text
+var newP = document.createElement('p'); // paragraph element
+
+newP.appendChild(newParText);// 'fill in' paragraph element with text
+document.querySelector('body').appendChild(newP); // 'place' the paragraph into the body
 
