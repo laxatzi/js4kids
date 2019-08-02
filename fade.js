48455ae0d813dc7565988e2a0fb18763d18f6ff1
laxatzi <laxatzi@yahoo.com>
2019-08-02 12:56:56 +0300

    add js code to toggle 'hide and show' img

--- fade.js
+++ fade.js
@@ -1,11 +1,14 @@
+// Show an element
 var show = function (elem) {
 	elem.style.display = 'block';
 };
 
+// Hide an element
 var hide = function (elem) {
 	elem.style.display = 'none';
 };
 
+// Toggle element visibility
 var toggle = function (elem) {
 
 	// If the element is visible, hide it
