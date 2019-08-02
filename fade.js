b60f03eca77d0e66ac69f7eb0c8c0dbd0507cc4d
laxatzi <laxatzi@yahoo.com>
2019-08-02 13:43:46 +0300

    add function to fade.js file for showing element

--- fade.js
+++ fade.js
@@ -1,6 +1,23 @@
 // Show an element
 var show = function (elem) {
-	elem.style.display = 'block';
+   
+   var getHeight = function () {
+      elem.style.display = 'block'; // Make it visible ... this would suffice on its own (out of the function) if didn't want to animate
+      var height = elem.scrollHeight + 'px'; // Get it's height...scrollHeight tells you the height of an element’s content, and store it to a variable. This returns an integer, so we’ll need to add px to the end to use with our inline style.
+      elem.style.display = ''; //  Hide it again
+      return height;  //Finally, we’ll hide our element again by removing the display property, and return our height value.
+   };  // end of function
+
+   var height = getHeight(); // Get the natural height
+	elem.classList.add('is-visible'); // Make the element visible
+   elem.style.height = height; // Update the max-height
+   
+      // Once the transition is complete, remove the inline max-height so the content can scale responsively
+      window.setTimeout(function () {
+         elem.style.height = '';
+      }, 350);
+
+
 };
 
 // Hide an element
@@ -47,3 +64,5 @@ var getHeight = function () {
 	elem.style.display = ''; //  Hide it again
 	return height;  //Finally, we’ll hide our element again by removing the display property, and return our height value.
 };
+
+// In the above function ...Even though we’re showing and then re-hiding our content, it never shows visibly for users because the function runs so quickly (just a few milliseconds) and thus never actually renders visually in the DOM.
