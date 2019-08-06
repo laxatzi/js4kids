f7c7c6ecd9758c87b1b2832aa79da6fb050ea541
laxatzi <laxatzi@yahoo.com>
2019-08-06 16:01:50 +0300

    recreate toggle function

--- fade2.js
+++ fade2.js
@@ -1,3 +1,38 @@
+var show = function(elem) {
+   var getHeight = function(p){
+      elem.style.display = "block";
+      var height = elem.scrollHeight+"px"
+      elem.style.display = "";
+      return height;
+   }
+
+   var height = getHeight();
+   elem.style.height = height;
+   elem.classList.add('is-visible');
+
+   window.setTimeout(function(){
+         elem.style.height = '';
+   }, 350);
+}
+
+
+var hide = function(elem) {
+   var getHeight= function(p) {
+       var height = elem.scrollHeight + "px";
+       return height;
+   }
+   var height = getHeight();
+   elem.style.height = height;
+
+   window.setTimeout(function() {
+      elem.style.height = "0";
+   }, 1);
+
+   window.setTimeout(function() {
+      elem.classList.remove('is-visible');
+   }, 350);
+}
+
 var toggle = function(elem) {
    if(elem.classList.contains('is-visible')){
       hide(elem);
@@ -6,7 +41,7 @@ var toggle = function(elem) {
 }
 
 document.addEventListener('click', function(event){
-   if(!document.event.target.classList.contains('toggle')) return;
+   if(!event.target.classList.contains('toggle')) return;
    
    event.preventDefault();
 
