fde5d874b9d9c0ce38fab5c50ce11095aba881eb
laxatzi <laxatzi@yahoo.com>
2019-08-06 12:16:43 +0300

    update fade2.html and fade2.js files

--- fade2.js
+++ fade2.js
@@ -1,4 +1,37 @@
+var show = function(elem){
+      var getHeight = function(){
+         elem.style.display = "block";
+         var height = elem.scrollHeight+"px";
+         elem.style.display = "";
+         return height;
+      };
+      var height = getHeight();
+      elem.style.height= height;
+      elem.classList.add('is-visible');
 
+      window.setTimeout(function() {
+            elem.style.height= '';
+      }, 350);
+};
+
+var hide = function (elem) {
+   // Give the element a height to change from
+   var getHeight = function(){
+      var height = elem.scrollHeight+ "px";
+      return height;
+   }
+   var height = getHeight();
+   elem.style.height = height;
+      //set our height back to 0. 
+      window.setTimeout(function () {
+         elem.style.height = '0'; // we could use without setTimeout() but...
+      }, 1); //if we run it too quickly, the browser won’t detect the change at all and won’t run. We’ll use setTimeout() with a 1 millisecond delay to make sure the change registers.
+   
+      // When the transition is complete, hide it
+	window.setTimeout(function () {
+            elem.classList.remove('is-visible');
+         }, 350);
+   };
 var toggle = function(elem) {
 // if element is visible => hide it
     if(elem.classList.contains('is-visible')){
