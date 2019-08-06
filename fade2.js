5192fe56f0256aa31fc6dde9c5df02a129d5a1c0
laxatzi <laxatzi@yahoo.com>
2019-08-06 10:42:04 +0300

    update fade2.js file

--- fade2.js
+++ fade2.js
@@ -1,1 +1,24 @@
+var show = function(element) {
 
+}
+
+var toggle =  function(element) {
+   if(element.classList.contains('is-visible')){
+      hide(element);
+      return;
+   }
+      show(element);
+}
+
+
+
+document.addEventListener('click', function(event){
+
+   if(!event.target.classList.contains('toggle')) return;
+   event.preventDefault;
+
+   var togglingContent = document.querySelector(event.target.hash);
+   if(!togglingContent) return; 
+
+   toggle(togglingContent);
+}, false);
