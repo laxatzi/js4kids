76000a392dff522f44a52f7f017e4db31f16fc03
laxatzi <laxatzi@yahoo.com>
2019-08-06 11:01:23 +0300

    create toggle function

--- fade2.js
+++ fade2.js
@@ -1,6 +1,19 @@
-document.addEventListener('click', function(ev){
-     
-   
+
+var toggle = function(elem) {
+   // if element is visible => hide it
+   if(elem.classList.contains('is-visible')){
+      hide(elem);
+      return;
+   }
+   // if not visible => show it
+   show(elem);
+}
+
+document.addEventListener('click', function(event){
+   // the link should contain the toggle class otherwise the function stops
+   if (!event.target.classList.contains("toggle")) return;
+   // link here works as a button for toggling
+    event.preventDefault();
    // store the toggling content value as a var
       var togglingContent =  document.querySelector(event.target.hash);
       // if event target doesn't include a hash 
