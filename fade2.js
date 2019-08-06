6128370d83f8ad7ed0185dc88eeaded397f29dac
laxatzi <laxatzi@yahoo.com>
2019-08-06 10:54:27 +0300

    add event listener with toggle function

--- fade2.js
+++ fade2.js
@@ -1,24 +1,11 @@
-var show = function(element) {
+document.addEventListener('click', function(ev){
+     
+   
+   // store the toggling content value as a var
+      var togglingContent =  document.querySelector(event.target.hash);
+      // if event target doesn't include a hash 
+      if (!togglingContent) return;
+      // the toggling function
+      toggle(togglingContent);
 
-}
-
-var toggle =  function(element) {
-   if(element.classList.contains('is-visible')){
-      hide(element);
-      return;
-   }
-      show(element);
-}
-
-
-
-document.addEventListener('click', function(event){
-
-   if(!event.target.classList.contains('toggle')) return;
-   event.preventDefault;
-
-   var togglingContent = document.querySelector(event.target.hash);
-   if(!togglingContent) return; 
-
-   toggle(togglingContent);
-}, false);
+}, {capture:false});
